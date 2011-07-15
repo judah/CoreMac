89bc897b532e4282fc895f23c689ab7b699be9a1
Judah Jacobson <judah.jacobson@gmail.com>
2011-07-15 16:18:05 -0700

    Documentation twiddle.

--- System/CoreGraphics/Font.hs
+++ System/CoreGraphics/Font.hs
@@ -21,7 +21,7 @@ fontWithDataProvider :: DataProvider -> IO Font
 fontWithDataProvider d = cfWith d $ \dp -> c_CGFontCreateWithDataProvider dp >>= retained
 
 type Glyph = CUShort -- TODO: use hsc2hs.  But need GHC's #5106 to be fixed first.
-                    -- (use <ApplicationServices/ApplicationServices.h>)
+                    -- (should use <ApplicationServices/ApplicationServices.h>)
 
 foreign import ccall unsafe "CGFontGetGlyphAdvances"
     c_CGFontGetGlyphAdvances :: FontRef -> Ptr Glyph -> CSize -> Ptr CInt -> IO CBool
