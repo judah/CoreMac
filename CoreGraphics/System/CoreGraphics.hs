2eb89bc0949208bbb1f1ecad853487bcd6c3dc00
Judah Jacobson <judah.jacobson@gmail.com>
2011-08-07 11:43:19 -0700

    Switch order of exports from CoreGraphics to be alphabetical.

--- CoreGraphics/System/CoreGraphics.hs
+++ CoreGraphics/System/CoreGraphics.hs
@@ -1,14 +1,14 @@
 module System.CoreGraphics (
+            module System.CoreGraphics.AffineTransform,
+            module System.CoreGraphics.Context,
             module System.CoreGraphics.DataProvider,
             module System.CoreGraphics.Font,
-            module System.CoreGraphics.Context,
             module System.CoreGraphics.Geometry,
-            module System.CoreGraphics.AffineTransform,
             ) where
 
+import System.CoreGraphics.AffineTransform
+import System.CoreGraphics.Context
 import System.CoreGraphics.DataProvider
 import System.CoreGraphics.Font
-import System.CoreGraphics.Context
 import System.CoreGraphics.Geometry
-import System.CoreGraphics.AffineTransform
 
