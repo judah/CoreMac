7b5a216e6b87388c2adc48f8834a7b69f8aff912
Judah Jacobson <judah.jacobson@gmail.com>
2011-08-09 19:33:18 -0700

    A basic CocoaFib project without any haskell.

---
+++ examples/FibTest/FibTest/FibTestAppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  FibTestAppDelegate.m
+//  FibTest
+//
+//  Created by Judah Jacobson on 8/9/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "FibTestAppDelegate.h"
+
+@implementation FibTestAppDelegate
+
+@synthesize window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ examples/FibTest/FibTest/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  FibTest
+//
+//  Created by Judah Jacobson on 8/9/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
