04d57e3fd9141a8a16dcdd4eec56a37cb6b424f3
Reiner Pope <reiner.pope@gmail.com>
2012-03-29 15:32:26 +1030

    Remove type signatures from importCFStringAs

--- CoreFoundation/System/CoreFoundation/String/TH.hs
+++ CoreFoundation/System/CoreFoundation/String/TH.hs
@@ -21,15 +21,25 @@ import Language.Haskell.TH
 
 declareCFType "String"
 
+{- | 
+Used for importing constant strings defined in C code. For example
+
+> importCFStringAs "kCFErrorDomainPOSIX" "domainPOSIX"
+
+Note that a type signature may be given and documentation added:
+
+> importCFStringAs "kCFErrorDomainPOSIX" "domainPOSIX"
+> -- | Error domain for POSIX errors
+> domainPOSIX :: ErrorDomain
+-}
 importCFStringAs :: Prelude.String -> Prelude.String -> Q [Dec]
 importCFStringAs foreignStr nameStr = do
     ptrName <- newName (nameStr ++ "Ptr")
     let name = mkName nameStr
     ptrType <- [t| Ptr StringRef |]
-    expr <- [| unsafePerformIO $ peek $(varE ptrName) >>= getAndRetain |]
+    expr <- [| unsafePerformIO $ peek $(varE ptrName) >>= getAndRetain :: String|]
     return
         [ ForeignD $ ImportF CCall Safe ("&" ++ foreignStr) ptrName ptrType
-        , SigD name (ConT ''String)
         , FunD name [Clause [] (NormalB expr) []]
         ]
 
